fda1e0db99ee7f6fde6024e98c55a4eb82b60870
Vincent <vkammerer@gmail.com>
2015-11-26 23:15:46 -0500

    Change public site nav animation

---
+++ public-site.js
@@ -0,0 +1,56 @@
+var init = function(){
+
+	var categorieNames = [
+		'introduction',
+		'installation',
+		'api',
+		'license'
+	]
+
+	categories = categorieNames.map(function(categoryName){
+		return {
+			name: categoryName,
+			expanded: false,
+			options: {
+				selector: '#' + categoryName,
+				sizeClass: 'expanded',
+				animation: {
+					duration: 300,
+					easing: 'cubic-bezier(0,0,0.32,1)'
+				}
+			}
+		}
+	})
+
+	var openCategory = function(category){
+		category.teleporter.element.parentElement.classList.add('selected');
+		category.teleporter.element.classList.add('active');
+		category.expanded = true;
+		return category.teleporter.teleport(['', 'expanded'])
+	}
+	var closeCategory = function(category){
+		category.teleporter.element.parentElement.classList.remove('selected');
+		category.teleporter.element.classList.remove('active');
+		category.expanded = false;
+		return category.teleporter.teleport(['expanded',''])
+	}
+
+	var onCategoryClick = function(selectedCategory){
+		var currentCategory = categories.filter(function(cat){
+			console.log(cat.expanded)
+			return cat.expanded
+		})[0]
+		closeCategory(currentCategory);
+		openCategory(selectedCategory);
+	}
+
+	categories.forEach(function(category){
+		category.teleporter = new Teleporter(category.options);
+		category.teleporter.element.addEventListener("click", function(){
+			if (category.expanded) { return }
+			onCategoryClick(category);
+		});
+	});
+	openCategory(categories[0])
+}
+window.onload = init;
