ae30b59e0bd78919d5273cd9b01cf7ef79039afa
Vincent <vkammerer@gmail.com>
2015-11-26 23:15:46 -0500

    Change public site nav animation

--- site/templates/index.js
+++ site/templates/index.js
@@ -13,6 +13,7 @@ export default function template(introduction, installation, api, license) {
 			<link rel="stylesheet" href="./table.css"/>
 			<link href='https://fonts.googleapis.com/css?family=Timmana' rel='stylesheet' type='text/css'>
 			<script src="teleporter-global-polyfilled.js"></script>
+			<script src="public-site.js"></script>
 			</head>
 			<body>
 				${ribon}
@@ -24,64 +25,34 @@ export default function template(introduction, installation, api, license) {
 					</header>
 					<div class="content">
 						<div class="section">
+							<div class="pagename">Introduction</div>
 							<div id="introduction" class="category">
 								${introduction}
 							</div>
 						</div>
 						<div class="section"></div>
 						<div class="section">
+							<div class="pagename">Installation</div>
 							<div id="installation" class="category">
 								${installation}
 							</div>
 						</div>
 						<div class="section"></div>
 						<div class="section">
+							<div class="pagename">API</div>
 							<div id="api" class="category">
 								${api}
 							</div>
 						</div>
 						<div class="section"></div>
 						<div class="section">
+							<div class="pagename">License</div>
 							<div id="license" class="category">
 								${license}
 							</div>
 						</div>
 					</div>
 				</div>
-				<script>
-					var init = function(){
-						[
-							'introduction',
-							'installation',
-							'api',
-							'license'
-						].forEach(function(category){
-								var myElement = new Teleporter({
-									selector: '#' + category,
-									sizeClass: 'normal',
-									animation: {
-										duration: 300,
-										easing: 'cubic-bezier(0,0,0.32,1)'
-									}
-								});
-								var normal = false;
-								myElement.element.addEventListener("click",function(){
-									myElement.element.parentElement.classList.add('selected');
-									myElement.element.classList.add('active')
-									myElement
-										.teleport(normal ? ['normal','thumbnail']: ['thumbnail','normal'])
-										.then(function(){
-											if (!normal) {
-												myElement.element.classList.remove('active')
-												myElement.element.parentElement.classList.remove('selected');
-											}
-										})
-									normal = !normal;
-								});
-							});
-					}
-					window.onload = init;
-				</script>
 			</body>
 		</html>
 	`
