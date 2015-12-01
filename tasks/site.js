c9013e615c07c61ab891529f45dff76a27b82643
Vincent <vkammerer@gmail.com>
2015-11-30 20:35:44 -0500

    Async all the things!

--- tasks/site.js
+++ tasks/site.js
@@ -9,7 +9,7 @@ let siteDirPath = path.join(__dirname, '..', 'site');
 export default function site() {
 	return new Promise((resolveSite, rejectSite) => {
 
-		let htmlPromises = [
+		let sitePromises = [
 			'introduction',
 			'installation',
 			'api',
@@ -17,35 +17,49 @@ export default function site() {
 		].map((p) => {
 			return new Promise((resolve, reject) => {
 				fs.readFile(path.join(__dirname, '..', 'docs', p + '.md'), 'utf-8', (err, data) => {
-					if (err) {
-						reject();
-						return;
+					if (!err) {
+						resolve(marked(data))
 					}
-					resolve(marked(data))
 				});
 			})
 		})
+
+		sitePromises.push(new Promise((resolve, reject) => {
+			fs.copy(
+				path.join(__dirname, '..', 'dist', 'teleporter-global-polyfilled.js'),
+				path.join(siteDirPath, 'static', 'scripts', 'teleporter-global-polyfilled.js'),
+				(err, data) => {
+					if (!err) {
+						resolve()
+					}
+				}
+			);
+		}))
+
+		sitePromises.push(new Promise((resolve, reject) => {
+			fs.copy(
+				require.resolve('normalize.css'),
+				path.join(siteDirPath, 'static', 'styles', 'normalize.css'),
+				(err, data) => {
+					if (!err) {
+						resolve()
+					}
+				}
+			);
+		}))
 
 		delete require.cache[require.resolve('../site/templates/index')]
 		delete require.cache[require.resolve('../site/templates/schema')]
 		delete require.cache[require.resolve('../site/templates/ribon')]
 		let indexTemplate = require('../site/templates/index').default;
 
-		Promise.all(htmlPromises).then((htmlSections) => {
+		Promise.all(sitePromises).then((siteValues) => {
+
+			let htmlSections = siteValues.slice(0, 4);
 
 			fs.writeFileSync(
 				path.join(siteDirPath, 'static', 'index.html'),
 				jsBeautify.html(indexTemplate(...htmlSections))
-			);
-
-			fs.copySync(
-				path.join(__dirname, '..', 'dist', 'teleporter-global-polyfilled.js'),
-				path.join(siteDirPath, 'static', 'scripts', 'teleporter-global-polyfilled.js')
-			);
-
-			fs.copySync(
-				require.resolve('normalize.css'),
-				path.join(siteDirPath, 'static', 'styles', 'normalize.css')
 			);
 
 			console.log('Site task complete'.cyan)
