3f9c75faa0ba5bb370308bc4843162daf0e3302b
Vincent <vkammerer@gmail.com>
2015-12-23 20:48:55 -0500

    Improve game

--- site/static/game/scripts/game.js
+++ site/static/game/scripts/game.js
@@ -1,17 +1,59 @@
 window.init = function(columnNum, lineNum){
 
+	var gameReady = false;
+
+	var renewImage = initStyles(columnNum, lineNum);
+
 	var tilesNum = columnNum * lineNum;
+	var scoresNode = document.querySelector('#scores');
+	var gameNode = document.querySelector('#game');
+	var tiles;
 
-	var tilesFragment = initTiles(tilesNum);
-	var layoutFragment = initLayout(columnNum, lineNum);
+	function initGame(){
+		gameReady = false;
+		scores.innerHTML = 'Look carefully!';
+		gameNode.innerHTML = '';
+		var tilesFragment = getTilesFragment(tilesNum);
+		var posFragment = getPosFragment(tilesNum);
 
-	document.querySelector('#game').appendChild(tilesFragment);
-	document.querySelector('#game').appendChild(layoutFragment);
+		gameNode.appendChild(tilesFragment);
+		gameNode.appendChild(posFragment);
 
-	var tiles = teleportTiles(tilesNum);
+		tiles = getTeleporterElements(tilesNum);
+		tiles.forEach(function(tile){
+			tile.teleporter.saveSteps(['pos_' + tile.index, 'pos_' + tile.shuffledIndex]);
+			tile.content.addEventListener('click', onTileClick.bind(this, tile));
+		})
+
+		tiles.forEach(function(tile){
+			tile.teleporter.teleport('pos_' + tile.index).then(function(){
+				tile.element.classList.add('initialized');
+			});
+		})
+
+		setTimeout(function(){
+			tiles.forEach(function(tile){
+				tile.teleporter.teleport(['pos_' + tile.index, 'pos_' + tile.shuffledIndex]);
+			})
+			scores.innerHTML = 'Click 2 tiles to move them';
+			gameReady = true;
+		}, 5000)
+
+	}
+
 	var selection = [];
 
-	function handleClick(tile) {
+	function onTileClick(tile) {
+		if (!gameReady) {
+			return;
+		}
+		if (
+			(selection.length === 1) &&
+			(selection[0] === tile.index)
+		) {
+			tile.element.classList.remove('selected');
+			return;
+		}
 		tile.element.classList.add('selected');
 		selection.push(tile.index);
 		if (selection.length === 2) {
@@ -22,6 +64,13 @@ window.init = function(columnNum, lineNum){
 			});
 			tile1.teleporter.teleport(['pos_' + tile1.shuffledIndex, 'pos_' + tile0.shuffledIndex]).then(function(){
 				tile1.element.classList.remove('selected');
+				if (isFinished()) {
+					scores.innerHTML = 'Yay!';
+					setTimeout(function(){
+						renewImage()
+						initGame()
+					}, 2000);
+				}
 			});
 			var tile0ShuffledIndex = tile0.shuffledIndex;
 			tile0.shuffledIndex = tile1.shuffledIndex;
@@ -30,23 +79,13 @@ window.init = function(columnNum, lineNum){
 		}
 	}
 
-	tiles.forEach(function(tile){
-		tile.teleporter.saveSteps(['pos_' + tile.index, 'pos_' + tile.shuffledIndex]);
-		tile.content.addEventListener('click', handleClick.bind(this, tile));
-	})
-
-	tiles.forEach(function(tile){
-		tile.teleporter.teleport('pos_' + tile.index).then(function(){
-			tile.element.classList.add('initialized');
+	function isFinished() {
+		return tiles.every(function(tile){
+			return (tile.index === tile.shuffledIndex)
 		});
-	})
+	}
 
-	setTimeout(function(){
-		tiles.forEach(function(tile){
-			tile.teleporter.teleport(['pos_' + tile.index, 'pos_' + tile.shuffledIndex]);
-		})
-
-	}, 5000)
+	initGame();
 
 	document.body.classList.add('navigation-ready');
 
